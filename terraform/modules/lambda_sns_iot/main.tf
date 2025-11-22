68181c7133a7ac9e53051ee2523d1bc4d3b84734
JBTTT <perseverancejb@hotmail.om>
2025-11-23 00:53:15 +0800

    Lambda Testing

--- terraform/modules/lambda_sns_iot/main.tf
+++ terraform/modules/lambda_sns_iot/main.tf
@@ -1,18 +1,28 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../../lambda_src"
-  output_path = "${path.module}/lambda.zip"
+  source_dir  = "${path.module}/../../lambda_src_iot"
+  output_path = "${path.module}/lambda_iot.zip"
+}
+
+resource "aws_sns_topic" "alert_topic" {
+  name = "${var.name_prefix}-iot-alert-topic"
+}
+
+resource "aws_sns_topic_subscription" "email_sub" {
+  topic_arn = aws_sns_topic.alert_topic.arn
+  protocol  = "email"
+  endpoint  = var.alert_email
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name = "${var.name_prefix}-lambda-role"
+  name = "${var.name_prefix}-iot-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action = "sts:AssumeRole",
+      Effect = "Allow",
       Principal = { Service = "lambda.amazonaws.com" },
-      Effect = "Allow"
+      Action = "sts:AssumeRole"
     }]
   })
 }
@@ -22,40 +32,55 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_lambda_function" "iot_handler" {
-  function_name = "${var.name_prefix}-iot_handler"
+resource "aws_iam_role_policy" "lambda_sns_policy" {
+  name = "${var.name_prefix}-lambda-sns-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["sns:Publish"],
+      Resource = [aws_sns_topic.alert_topic.arn]
+    }]
+  })
+}
+
+# ⭐ THIS IS THE LAMBDA RESOURCE NAME YOU MUST USE
+resource "aws_lambda_function" "alert_lambda" {
+  function_name = "${var.name_prefix}-iot-handler"
   filename      = data.archive_file.lambda_zip.output_path
   handler       = "index.handler"
-  runtime       = "nodejs18.x"
+  runtime       = "python3.11"
   role          = aws_iam_role.lambda_role.arn
+  source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
+
+  environment {
+    variables = {
+      SNS_TOPIC_ARN = aws_sns_topic.alert_topic.arn
+    }
+  }
 }
 
-resource "aws_sns_topic" "alert_topic" {
-  name = "${var.name_prefix}-alert-topic"
-}
+# ⭐ THIS IS THE EVENT RULE NAME YOU MUST USE
+resource "aws_cloudwatch_event_rule" "event_rule" {
+  name        = "${var.name_prefix}-iot-event"
+  description = "Simulated IoT device events"
 
-resource "aws_sns_topic_subscription" "email_sub" {
-  topic_arn = aws_sns_topic.alert_topic.arn
-  protocol  = "email"
-  endpoint  = var.alert_email
-}
-
-resource "aws_cloudwatch_event_rule" "iot_rule" {
-  name        = "${var.name_prefix}-event"
-  description = "Triggers Lambda"
   event_pattern = jsonencode({
-    "source" : ["aws.ec2"]
+    "source": ["cet11.grp1.iot"],
+    "detail-type": ["iot.telemetry"]
   })
 }
 
 resource "aws_cloudwatch_event_target" "event_target" {
-  rule      = aws_cloudwatch_event_rule.iot_rule.name
-  target_id = "lambda-target"
+  rule      = aws_cloudwatch_event_rule.event_rule.name
+  target_id = "lambda-iot-target"
   arn       = aws_lambda_function.alert_lambda.arn
 }
 
 resource "aws_lambda_permission" "allow_events" {
-  statement_id  = "AllowExecutionFromEvents"
+  statement_id  = "AllowExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.alert_lambda.function_name
   principal     = "events.amazonaws.com"
--- terraform/modules/lambda_sns_iot/outputs.tf
+++ terraform/modules/lambda_sns_iot/outputs.tf
@@ -1,11 +1,11 @@
-output "sns_topic_arn" {
-  value = aws_sns_topic.alert_topic.arn
-}
-
 output "lambda_name" {
-  value = aws_lambda_function.iot_handler.function_name
+  value = aws_lambda_function.alert_lambda.function_name
 }
 
 output "event_rule_name" {
-  value = aws_cloudwatch_event_rule.iot_rule.name
+  value = aws_cloudwatch_event_rule.event_rule.name
+}
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.alert_topic.arn
 }
