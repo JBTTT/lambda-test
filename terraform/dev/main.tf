de5572660b116c223498c04f1abeb6078dfad5d1
JBTTT <perseverancejbt@hotmail.com>
2025-11-23 14:07:08 +0800

    Lambda Test

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -7,18 +7,24 @@ terraform {
   }
 }
 
+variable "region" {
+  description = "AWS region for deployment"
+  type        = string
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 module "lambda_sns_iot" {
   source      = "../modules/lambda_sns_iot"
-  name_prefix = "cet11-grp1-dev"
+  name_prefix = "cet11-grp1-prod"
   alert_email = "perseverancejb@hotmail.com"
+  region      = var.region
 }
 
 module "iot_simulator_ec2" {
   source      = "../modules/iot_simulator_ec2"
-  name_prefix = "cet11-grp1-dev"
-  region      = "us-east-1"
+  name_prefix = "cet11-grp1-prod"
+  region      = var.region
 }
--- terraform/dev/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "region" {
-  description = "AWS region for deployment"
-  type        = string
-}
